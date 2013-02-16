26da7034d6232a7100f0e933b5402ea737d75fd7
Thomas Levine <occurrence@thomaslevine.com>
2013-02-16 12:19:22 -0500

    aoeu

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p downloads
+wget -O downloads/us.html http://www.nysedregents.org/USHistoryGov/home.html
+wget -O downloads/global.html http://www.nysedregents.org/GlobalHistoryGeography/
