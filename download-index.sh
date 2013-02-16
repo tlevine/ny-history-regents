951827f5d3360617dfc11e54ec72a5d813885cea
Thomas Levine <occurrence@thomaslevine.com>
2013-02-16 12:29:03 -0500

    aoeu

--- download-index.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir -p downloads
-wget -O downloads/us.html http://www.nysedregents.org/USHistoryGov/home.html
-wget -O downloads/global.html http://www.nysedregents.org/GlobalHistoryGeography/
---
+++ download.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+'Download the tests and answers given the index file.'
+import lxml.html
+import os
+from urllib import urlretrieve
+
+def download_parse_index(index_url, index_filename):
+    if not os.path.isfile(index_filename):
+        urlretrieve(index_url, filename = index_filename)
+
+    html = lxml.html.parse(index_filename)
+    html.getroot().make_links_absolute(index_url)
+    print html.xpath('//a/@href')
+
+def main():
+    download_parse_index('http://www.nysedregents.org/USHistoryGov/home.html', 'us.html')
+    download_parse_index('http://www.nysedregents.org/GlobalHistoryGeography', 'global.html')
+
+if __name__ == '__main__':
+    main()
