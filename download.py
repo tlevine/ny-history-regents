854ecc004e325871c5151efb1a8755d349f4158d
Thomas Levine <occurrence@thomaslevine.com>
2013-02-16 12:41:17 -0500

    hardhat

--- download.py
+++ download.py
@@ -6,15 +6,15 @@ from urllib import urlretrieve
 import h.hardhat as hardhat
 
 def index_to_files(index_url):
-    hardhat.get(index_url, 'downloads')
-    html = lxml.html.parse(index_filename)
-    html.getroot().make_links_absolute(index_url)
+    raw = hardhat.get(index_url, 'downloads')
+    html = lxml.html.fromstring(raw)
+    html.make_links_absolute(index_url)
     test_urls = filter(lambda href: href[-4:] in ('.pdf', '.xls'), html.xpath('//a/@href'))
     return test_urls
 
 def main():
-    download_parse_index('http://www.nysedregents.org/USHistoryGov/home.html', 'us.html')
-#   download_parse_index('http://www.nysedregents.org/GlobalHistoryGeography', 'global.html')
+    print index_to_files('http://www.nysedregents.org/USHistoryGov/home.html')
+    print index_to_files('http://www.nysedregents.org/GlobalHistoryGeography')
 
 if __name__ == '__main__':
     main()
