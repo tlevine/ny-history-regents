cab5c6c86ac33610119633450f2a58fad8832801
Thomas Levine <occurrence@thomaslevine.com>
2013-02-16 12:39:33 -0500

    hardhat

--- download.py
+++ download.py
@@ -3,14 +3,14 @@
 import lxml.html
 import os
 from urllib import urlretrieve
+import h.hardhat as hardhat
 
-def download_parse_index(index_url, index_filename):
-    if not os.path.isfile(index_filename):
-        urlretrieve(index_url, filename = index_filename)
-
+def index_to_files(index_url):
+    hardhat.get(index_url, 'downloads')
     html = lxml.html.parse(index_filename)
     html.getroot().make_links_absolute(index_url)
     test_urls = filter(lambda href: href[-4:] in ('.pdf', '.xls'), html.xpath('//a/@href'))
+    return test_urls
 
 def main():
     download_parse_index('http://www.nysedregents.org/USHistoryGov/home.html', 'us.html')
