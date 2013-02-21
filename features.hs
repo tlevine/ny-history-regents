9f8d9ab4225574ab9d1e6c7564696ba487f12369
Thomas Levine <occurrence@thomaslevine.com>
2013-02-21 17:31:09 -0500

    length of answer seems to matter

--- features.hs
+++ features.hs
@@ -14,7 +14,9 @@ data Answer = Answer { file :: String
                      , isCorrect :: Bool
            
                      -- Features?
-                     --, hasAbsolutes :: Bool
+                     , nCharacters :: Maybe Int
+                     , nWords :: Maybe Int
+                     , sumLevenshtein :: Maybe Int
 } deriving (Show)
 
 -- All of the answers for a question
@@ -28,12 +30,12 @@ levenshtein s t = d!!(length s)!!(length t)
     distance 0 j = j
     distance i j = minimum [d!!(i-1)!!j+1, d!!i!!(j-1)+1, d!!(i-1)!!(j-1) + (if s!!(i-1)==t!!(j-1) then 0 else 1)]
 
-sumLevenshtein :: Question -> Answer -> Int
-sumLevenshtein answers thisAnswer = sum $ map (levenshtein $ answer thisAnswer) $ map answer answers
+getSumLevenshtein :: Question -> Answer -> Int
+getSumLevenshtein answers thisAnswer = sum $ map (levenshtein $ answer thisAnswer) $ map answer answers
   where
     thisAnswerStr = answer thisAnswer
 
-questionQuery = "SELECT DISTINCT examfile, \"number\" FROM answer LIMIT 2;"
+questionQuery = "SELECT DISTINCT examfile, \"number\" FROM answer LIMIT 3;"
 answerQuery = "SELECT examfile, \"number\", choice, question, answer, isCorrect FROM answer WHERE examfile = ? AND \"number\" = ?;"
 
 convAnswer :: [SqlValue] -> Answer
@@ -48,7 +50,7 @@ convAnswer [examfile, number, choice, question, answer, isCorrect] = Answer { fi
 guess :: Question -> (Int, Bool)
 guess answers = (predictedChoice, predictionCorrect)
   where
-    distances = map (sumLevenshtein answers) answers
+    distances = map (getSumLevenshtein answers) answers
     dMax = foldl max 0 distances
     dMin = foldl min dMax distances
     predictedChoice = fst $ head $ filter (\these -> snd these == dMin) $ zip [1..(length answers)] $ distances
@@ -85,7 +87,9 @@ main = do
   --putStrLn $ show $ score results
 
   --Word counts
-  putStrLn $ show $ foldl (M.unionWith (+)) M.empty $ map wordCount $ map answer $ head questions
+  --putStrLn $ show $ foldl (M.unionWith (+)) M.empty $ map wordCount $ map answer $ head questions
+  
+  putStrLn $ show $ map (length . answer) $ head questions
 
   -- And disconnect from the database
   disconnect conn
