d06ff66a818e9f6aa98f43ed634d6e4eb67abd85
Thomas Levine <occurrence@thomaslevine.com>
2013-02-24 16:37:41 -0500

    oeu

--- predict.r
+++ predict.r
@@ -1,15 +1,18 @@
+#!/usr/bin/env Rscript
 library(sqldf)
 library(reshape2)
 library(ggplot2)
 
-a <- (function() {
+a <- function(normalize = F) {
   a.wide <- sqldf('SELECT * FROM answer;', dbname = '/tmp/history-regents.db')
 
   # Normalize
-  features <- c('sumLevenshtein', 'nCharacters', 'nWords', 'containsCommonWord', 'isQualitativeAnswerAboutGraph')
-  a.wide[features] <- lapply(a.wide[features], function(f) {
-    (f - mean(f)) / sd(f)
-  })
+  if (normalize) {
+    features <- c('sumLevenshtein', 'nCharacters', 'nWords', 'containsCommonWord', 'isQualitativeAnswerAboutGraph')
+    a.wide[features] <- lapply(a.wide[features], function(f) {
+      (f - mean(f)) / sd(f)
+    })
+  }
 
   a.long <- melt(a.wide, c('examfile', 'number', 'choice', 'isCorrect'), variable.name = 'feature')
   a.long$isCorrect <- as.logical(a.long$isCorrect)
@@ -17,12 +20,16 @@ a <- (function() {
   a.long$basename <- sapply(a.long$examfile, function(fn) {rev(strsplit(fn, '/')[[1]])[1]})
 
   a.long
-})()
+}
 
-p.features <- ggplot(a) + aes(x = value, fill = factor(isCorrect)) +
+p.features <- ggplot(a(normalize = F)) + aes(x = value, fill = factor(isCorrect)) +
   geom_histogram(position = 'dodge') + facet_grid(. ~ feature, scales = 'free')
 
-p.questions <- dlply(a, c('examfile', 'number'), function(df){
+pdf('features.pdf', width = 11, height = 8.5)
+print(p.features)
+dev.off()
+
+p.questions <- dlply(a(normalize = T), c('examfile', 'number'), function(df){
   ggplot(df) + aes(x = choice, y = value, color = isCorrect) +
     scale_color_manual(values=c("#000000", "#FF0000")) +
     facet_grid(. ~ feature, scale = 'free', space = 'free') + geom_point() +
